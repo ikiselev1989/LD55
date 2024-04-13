9d4c7d88726252c1658301fc444cfd0d2cdb4b55
Ilya Kiselev <ikiselev1989@gmail.com>
2024-04-13 21:09:12 +0300

    mvp

--- src/game/components/objects/candle.ts
+++ src/game/components/objects/candle.ts
@@ -3,9 +3,10 @@ import { Actor, Sprite, Vector } from 'excalibur';
 import res from '@/res';
 import { Assets } from '@/game/resources/assets';
 import { TAGS } from '@/enums';
+import { random } from '@/game/utils';
 
 export default class Candle extends Actor {
-	private sprite = <Sprite>res.assets.getFrameSprite(Assets.CANDLES__1);
+	private sprite = <Sprite>res.assets.getFrameSprite(random.pickOne([Assets.CANDLES__1, Assets.CANDLES__2, Assets.CANDLES__3]));
 
 	constructor(props: ActorArgs) {
 		super(props);
