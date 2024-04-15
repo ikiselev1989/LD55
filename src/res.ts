6e8f1a273f3e5096c6c9056150ae810c0e77f028
Ilya Kiselev <ikiselev1989@gmail.com>
2024-04-16 02:18:52 +0300

    mvp

--- src/res.ts
+++ src/res.ts
@@ -3,7 +3,7 @@ import { ImageSource } from 'excalibur';
 import JSONSpriteSheetAnimation from '@/game/partials/json-spritesheet-animation';
 
 export default {
-	assets: new JSONSpriteSheet('/assets/assets.json'),
-	animation: new JSONSpriteSheetAnimation(['/assets/animations.json']),
-	noise: new ImageSource('/assets/noise.png'),
+	assets: new JSONSpriteSheet('assets/assets.json'),
+	animation: new JSONSpriteSheetAnimation(['assets/animations.json']),
+	noise: new ImageSource('assets/noise.png'),
 };
